90673a28bd93fb31352416fd421e6cabe7b0b173
Your Name <you@example.com>
2024-08-17 02:56:42 -0930

    eight commit

---
+++ app.js
@@ -0,0 +1,94 @@
+alert("welcome!")
+
+//quiz1
+var question1 = prompt("What's your name?");
+if(question1 === question1){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+//quiz2
+var question1 = prompt("What is pakistan official name?");
+if(question1 === " The Islamic Republic Of Pakistan"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+//quiz3
+var question1 = prompt("What is pakistan Official Language?");
+if(question1 === "Urdu"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+//quiz4
+var question1 = prompt("Where the biggest salt mine located in pakistan?");
+if(question1 === "Mangora"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+//quiz5
+var question1 = prompt("What is the national Fruit of pakistan?");
+if(question1 === "Mango"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+//quiz6
+var question1 = prompt("What is the national animal of pakistan?");
+if(question1 === "Markhor"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+//quiz7
+var question1 = prompt("What is the national game of pakistan?");
+if(question1 === "Hockey"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+//quiz8
+var question1 = prompt("What is the national dish of Pakistan?");
+if(question1 === "Biryani"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+
+//quiz9
+var question1 = prompt("What is the capital of pakistan?");
+if(question1 === "Islamabad"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+
+//quiz10
+var question1 = prompt("Who is founder of pakistan?");
+if(question1 === "Muhammad Ali Jinnah"){
+    alert("correct answer");
+}else{
+    alert("Wrong answer!");
+}
+
+alert("Thanks!")
+
